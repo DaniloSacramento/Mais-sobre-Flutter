d5530dcbac9a1fa6751f2be620e84a6ddd9c7502
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-16 00:56:41 -0300

    -

--- curso_/android/app/src/main/kotlin/com/example/curso_/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.curso_
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ curso__/lib/primeira_tela.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       color: Colors.black,
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+   crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          Row(
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
+   crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),Container(
+                color: Colors.yellow,
+                width: 100,
+                height: 100,
+              ),Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+   crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              ),
+               Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+               Container(
+                color: Colors.yellow,
+                width: 100,
+                height: 100,
+              )
+             ],
+          ),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+   crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              ),
+               Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+               Container(
+                color: Colors.yellow,
+                width: 100,
+                height: 100,
+              )
+             ],
+          )
+        ],
+       ),
+    );
+  }
+}
