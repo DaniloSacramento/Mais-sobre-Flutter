6d1620b77ec7cccea00f54c81ff01e0ccd89ba4b
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-20 21:52:23 -0300

    formulario 2

--- curso__/lib/screens/form_screen.dart
+++ curso__/lib/screens/form_screen.dart
@@ -20,67 +20,99 @@ class _FormularioState extends State<Formulario> {
       appBar: AppBar(
         title: Text("Nova Tarefa"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 650,
-            width: 375,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 3)
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: nameController,
-                    decoration: const  InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Nome",
-                      fillColor: Colors.white70,
-                      filled: true
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Center(
+          
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: const  InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Nome",
+                        fillColor: Colors.white70,
+                        filled: true
+                      ),
+                      
                     ),
                     
                   ),
-                  
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: dificuldadeController,
-                    decoration: const  InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Dificuldade",
-                      fillColor: Colors.white70,
-                      filled: true
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: dificuldadeController,
+                      decoration: const  InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Dificuldade",
+                        fillColor: Colors.white70,
+                        filled: true
+                      ),
+                      
                     ),
                     
                   ),
-                  
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: imageController,
-                    decoration: const  InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Image",
-                      fillColor: Colors.white70,
-                      filled: true
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      onChanged: (text){
+                        setState(() {
+                          
+                        });
+                      },
+                      controller: imageController,
+                      decoration: const  InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Image",
+                        fillColor: Colors.white70,
+                        filled: true
+                      ),
+                      
                     ),
                     
                   ),
-                  
-                ),
-                ElevatedButton(onPressed: (){
-                  print(nameController.text);
-                   print(int.parse(dificuldadeController.text));
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.red)
+                    ),
+                    child: ClipRRect(borderRadius: BorderRadius.circular(10),
+                    child: Image.network(imageController.text, 
+                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                      return Image.asset('assets/images/nophoto.png');
+                    },
+                    fit: BoxFit.cover,
+
+                    ),
+                    
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){
+                    print(nameController.text);
+                    print(int.parse(dificuldadeController.text));
                     print(imageController.text);
-                }, child: Text("Adicionar"))
-              ],
+                  }, child: Text("Adicionar"),
+                  
+                  )
+                ],
+              ),
             ),
           ),
         ),
