a35d96c40326f18a0f1f55f40a4948e263391ffa
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-20 21:22:56 -0300

    form

--- curso__/lib/screens/form_screen.dart
+++ curso__/lib/screens/form_screen.dart
@@ -10,6 +10,10 @@ class Formulario extends StatefulWidget {
 }
 
 class _FormularioState extends State<Formulario> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController dificuldadeController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,18 +21,67 @@ class _FormularioState extends State<Formulario> {
         title: Text("Nova Tarefa"),
       ),
       body: Center(
-        child: Container(
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3)
-          ),
-          child: Column(
-            children: [
-              
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 3)
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: const  InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Nome",
+                      fillColor: Colors.white70,
+                      filled: true
+                    ),
+                    
+                  ),
+                  
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: dificuldadeController,
+                    decoration: const  InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Dificuldade",
+                      fillColor: Colors.white70,
+                      filled: true
+                    ),
+                    
+                  ),
+                  
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: imageController,
+                    decoration: const  InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Image",
+                      fillColor: Colors.white70,
+                      filled: true
+                    ),
+                    
+                  ),
+                  
+                ),
+                ElevatedButton(onPressed: (){
+                  print(nameController.text);
+                   print(int.parse(dificuldadeController.text));
+                    print(imageController.text);
+                }, child: Text("Adicionar"))
+              ],
+            ),
           ),
         ),
       ),
