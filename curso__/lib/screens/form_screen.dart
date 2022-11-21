020f56a178578bb85d7a46c8c5e2fafe586a17a3
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-20 22:55:12 -0300

    validações

--- curso__/lib/screens/form_screen.dart
+++ curso__/lib/screens/form_screen.dart
@@ -14,104 +14,130 @@ class _FormularioState extends State<Formulario> {
   TextEditingController dificuldadeController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Nova Tarefa"),
-      ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Center(
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Nova Tarefa"),
+        ),
+        body: Center(
           
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 650,
-              width: 375,
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(width: 3)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: nameController,
-                      decoration: const  InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Nome",
-                        fillColor: Colors.white70,
-                        filled: true
+            child: SingleChildScrollView(
+              child: Container(
+                height: 650,
+                width: 375,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 3)
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (String ? value){
+                          if(value != null && value.isEmpty){
+                            return "Insira o nome da Tarefa";
+                          }
+                          return null;
+                        },
+                        controller: nameController,
+                        decoration: const  InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Nome",
+                          fillColor: Colors.white70,
+                          filled: true
+                        ),
+                        
                       ),
                       
                     ),
-                    
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: dificuldadeController,
-                      decoration: const  InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Dificuldade",
-                        fillColor: Colors.white70,
-                        filled: true
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty || int.parse(value ) > 5 || int.parse(value) < 1){
+                            return "Insira uma Dificuldade entre 1 e 5";
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.number,
+                        controller: dificuldadeController,
+                        decoration: const  InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Dificuldade",
+                          fillColor: Colors.white70,
+                          filled: true
+                        ),
+                        
                       ),
                       
                     ),
-                    
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      onChanged: (text){
-                        setState(() {
-                          
-                        });
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+    
+                        onChanged: (text){
+                          setState(() {
+                            
+                          });
+                        },
+                        validator: (value){
+                          if(value!.isEmpty){
+                           return "Insira um URL de Imagem!"; 
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.url,
+                        controller: imageController,
+                        decoration: const  InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Image",
+                          fillColor: Colors.white70,
+                          filled: true
+                        ),
+                        
+                      ),
+                      
+                    ),
+                    Container(
+                      height: 100,
+                      width: 72,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(width: 2, color: Colors.red)
+                      ),
+                      child: ClipRRect(borderRadius: BorderRadius.circular(10),
+                      child: Image.network(imageController.text, 
+                      errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                        return Image.asset('assets/images/nophoto.png');
                       },
-                      controller: imageController,
-                      decoration: const  InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Image",
-                        fillColor: Colors.white70,
-                        filled: true
+                      fit: BoxFit.cover,
+            
                       ),
                       
+                      ),
                     ),
+                    ElevatedButton(onPressed: (){
+                      if(_formKey.currentState!.validate()){
+                      print(nameController.text);
+                      print(int.parse(dificuldadeController.text));
+                      print(imageController.text);
+                      }
+                    }, child: Text("Adicionar"),
                     
-                  ),
-                  Container(
-                    height: 100,
-                    width: 72,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(width: 2, color: Colors.red)
-                    ),
-                    child: ClipRRect(borderRadius: BorderRadius.circular(10),
-                    child: Image.network(imageController.text, 
-                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
-                      return Image.asset('assets/images/nophoto.png');
-                    },
-                    fit: BoxFit.cover,
-
-                    ),
-                    
-                    ),
-                  ),
-                  ElevatedButton(onPressed: (){
-                    print(nameController.text);
-                    print(int.parse(dificuldadeController.text));
-                    print(imageController.text);
-                  }, child: Text("Adicionar"),
-                  
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
