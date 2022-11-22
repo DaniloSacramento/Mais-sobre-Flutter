76faf5afede846d8c31975214f2e3814014c8143
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-21 23:09:00 -0300

    lendo dados de um CRUD

--- curso__/lib/data/task_dao.dart
+++ curso__/lib/data/task_dao.dart
@@ -1,12 +1,50 @@
-class TaskDao{
- static const String tableSql = 'CREATE TABLE $_tablename('
-'$_name TEXT, '
-'$_difficulty INTEGER,' 
-'$_image TEXT)';
+import 'package:curso__/data/database.dart';
+import 'package:curso__/primeira_tela.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TaskDao {
+  static const String tableSql = 'CREATE TABLE $_tablename('
+      '$_name TEXT, '
+      '$_difficulty INTEGER,'
+      '$_image TEXT)';
 
   static const String _tablename = "taskTable";
   static const String _name = "nome";
   static const String _image = "imagem";
   static const String _difficulty = "dificuldade";
 
+  save(Task tarefa) async {}
+  Future<List<Task>> findAll() async {
+    print("Acessando o findAll");
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
+    print("Procurando DADOS $result");
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> listaDeTarefas) {
+    print("Convertendo TO LIST");
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in listaDeTarefas) {
+      final Task tarefa = Task(
+          nome: linha[_name],
+          foto: linha[_image],
+          dificuldade: linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    print("Lista de Tarefas $tarefas");
+    return tarefas;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    print("Acessando o find");
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados
+        .query(_tablename, where: "$_name = ? ", whereArgs: [nomeDaTarefa],);
+        print("Tarefa encontrada: ${toList(result)}");
+        return toList(result);
+  }
+
+  delete(String nomeDaTarefa) async {}
 }
